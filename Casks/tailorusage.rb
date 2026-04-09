a9dd2aec34dc34bb3c74da4ce6602e79b5604ddf
Nikolaas Verlee <niko.verlee@tailor-hub.com>
2026-04-09 16:22:02 +0200

    chore: update tailorusage to v0.5.0

--- Casks/tailorusage.rb
+++ Casks/tailorusage.rb
@@ -1,6 +1,6 @@
 cask "tailorusage" do
-  version "0.4.1"
-  sha256 "902f26fa2a1c9d4616ce34657dc636388da422cfc6e81f1483f41d86da3c81e1"
+  version "0.5.0"
+  sha256 "5bb007811a5ec832d29d282c91395a5f23e037530a2f9a61802e2666843321b0"
 
   url "https://github.com/TailorHub-Mad/TailorUsage/releases/download/v#{version}/TailorUsage_#{version}_aarch64.dmg"
   name "TailorUsage"
